edc9d6af73eaf653d2204563d7ad48a9e4b451e9
baidakovil <baidakovil@gmail.com>
2023-12-13 20:07:46 +0300

    upd: no logger, docs, lic

--- main.py
+++ main.py
@@ -1,75 +1,38 @@
-import os
-from dotenv import load_dotenv
+# wbmon — Wildberries marketplace price monitor with Google Sheets publishing.
+# Copyright (C) 2023 Ilia Baidakov <baidakovil@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <https://www.gnu.org/licenses/>.
+"""This is the main program file: loading logger, environment, running the scheduler."""
+
 import logging
-from logging.handlers import RotatingFileHandler
 
-from scheduler import get_scheduler
+from dotenv import load_dotenv
 
-BOT_FOLDER = os.path.dirname(os.path.realpath(__file__))
-load_dotenv(os.path.join(BOT_FOLDER, '.env'))
+from parsing.scheduler import get_scheduler
+from services.logger import logger
+
+load_dotenv('.env')
+
+logger = logging.getLogger('A.ma')
+logger.setLevel(logging.DEBUG)
 
 
-def startLogger():
+def main() -> None:
     """
-    Loggers definition. 
-    Logger in main.py is highest logger (A), other are descendants (A.B, A.C)
-    """
-    logger = logging.getLogger('A')
-    logger.setLevel(logging.DEBUG)
-
-    class ContextFilter(logging.Filter):
-        """
-        Necessary to escape message dublications in console in
-        case when logger.setLevel of A logger lower then A.B (debugging case)
-        """
-
-        def filter(self, record):
-            if (record.name == 'A') and (record.levelname == 'DEBUG'):
-                return 0
-            else:
-                return 1
-    #  Logging to console.
-    ch = logging.StreamHandler()
-    #  Logging to file since last run (debugging case).
-    fh = logging.FileHandler(
-        filename=os.path.join(BOT_FOLDER, 'logger.log'),
-        mode='w')
-    #  Logging to file, continuous after bot restart.
-    rh = RotatingFileHandler(
-        filename=os.path.join(BOT_FOLDER, 'data/log/logger_rotating.log'),
-        mode='a',
-        maxBytes=1024*1024*20,
-        backupCount=5)
-    ch_formatter = logging.Formatter(
-        '[%(asctime)s.%(msecs)03d - %(name)3s - %(levelname)8s - %(funcName)18s()] %(message)s',
-        '%H:%M:%S')
-    fh_formatter = logging.Formatter(
-        '[%(asctime)s.%(msecs)03d - %(name)20s - %(filename)20s:%(lineno)4s - %(funcName)20s() - %(levelname)8s - %(threadName)10s] %(message)s',
-        '%Y-%m-%d %H:%M:%S')
-    ch.setLevel(logging.DEBUG)
-    fh.setLevel(logging.DEBUG)
-    rh.setLevel(logging.DEBUG)
-    ch.setFormatter(ch_formatter)
-    fh.setFormatter(fh_formatter)
-    rh.setFormatter(fh_formatter)
-    logger.addHandler(ch)
-    logger.addHandler(fh)
-    logger.addHandler(rh)
-    ch_filter = ContextFilter()
-    ch.addFilter(ch_filter)
-    return logger
-
-
-logger = startLogger()
-
-
-def main():
-    """
-    Function to run bot.
+    Function to run monitoring. Opa.
     """
     scheduler = get_scheduler()
     try:
-        logger.info('Bot run.')
+        logger.info('Program run now')
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
         pass
